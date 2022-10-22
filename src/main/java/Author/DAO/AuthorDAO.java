56cabf9e5b1d727cb0789ebd148e1e33c55765cb
indaffa <115632792+indaffa@users.noreply.github.com>
2022-10-22 16:32:28 +0800

    Add files via upload

--- src/main/java/Author/DAO/AuthorDAO.java
+++ src/main/java/Author/DAO/AuthorDAO.java
@@ -1,1 +1,37 @@
+package Author.DAO;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import dbconnection.DbConnection;
+
+public class AuthorDAO{
+    
+    public ArrayList<String> dropdownlist() throws SQLException {
+            
+            ArrayList<String> listAuthor = new ArrayList<>();
+             
+            try (Connection connection = DbConnection.init()) {
+                String sql = "SELECT username FROM author;";
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery(sql);
+                 
+                while (result.next()) {
+                    String name = result.getString("username");
+                    listAuthor.add(name);
+                }          
+                 
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                throw ex;
+            }      
+             
+            return listAuthor;
+        }
+}
