9d849c8339ff9717c87b5c4bc3fac587b87279a5
indaffa <115632792+indaffa@users.noreply.github.com>
2022-10-22 16:33:09 +0800

    Add files via upload

--- src/main/java/Author/Entity/Author.java
+++ src/main/java/Author/Entity/Author.java
@@ -1,59 +1,50 @@
 package Author.Entity;
 
+import java.io.File;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import general.Entity.*;
+import Author.DAO.*;
 
 
 public class Author extends User {
-    private String profileName;
-    private String description;
     
-    
-    
-    public Author(String username, String fullname, String password, String email, String profileName) {
-        super(username, fullname, password, email);
-        this.profileName = profileName;
-    }
+    private AuthorDAO authorDao = new AuthorDAO();
 
-
-    
-    public Author(String profileName, String description) {
+    public Author() {
         super();
-        this.profileName = profileName;
-        this.description = description;
-    }
-
-
-
-    public Author(String username, String password, String profileName) {
-        super(username, password);
-        this.profileName = profileName;
-    }
-
-    public Author(String username, String password, String profileName, String description) {
-        super(username, password);
-        this.profileName = profileName;
-        this.description = description;
-    }
-
-    public String getProfileName() {
-        return profileName;
-    }
-
-    public void setProfileName(String profileName) {
-        this.profileName = profileName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+        // TODO Auto-generated constructor stub
     }
     
     
+    public ArrayList<String> readReviews(){
+        return new ArrayList<String>();
+    }
     
+    public ArrayList<String> dropDownList() throws SQLException{
+        return authorDao.dropdownlist();
+    }
     
+    public void deletePaper(String papername) {
+        
+    }
     
+    public ArrayList<Blob>viewOwnPapers(String username){
+        return new ArrayList<Blob>();
+    }
     
+    public boolean updateAccount(String username, String fullname, String password,  String email, String ProfileName) {
+        return true;
+    }
+    
+    public ArrayList<Author>getCoAuthors(){
+        return new ArrayList<Author>();
+    }
+    
+    public boolean updatePaper(File file, String name, String author) {
+        return true;
+    }
 }
