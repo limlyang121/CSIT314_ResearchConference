23fe0cc5985afa7e70a0abdaf6dc1cdccec837e8
indaffa <115632792+indaffa@users.noreply.github.com>
2022-10-22 16:42:27 +0800

    Add files via upload

--- Db/db.sql
+++ Db/db.sql
@@ -4,79 +4,144 @@ create database csit314;
 
 use csit314;
 
-DROP TABLE IF EXISTS `systemadmin`;
-CREATE TABLE `systemadmin` (
-	`username` varchar(50) PRIMARY KEY not null,
-	`fullname` varchar(50) not null,
-	`password` varchar(50) not null,
-	`email` varchar(70) not null,
-	`profilename` varchar(50) not null
-);
-
-DROP TABLE IF EXISTS `reviewer`;
-CREATE TABLE `reviewer` (
-	`username` varchar(50) PRIMARY KEY not null,
-	`fullname` varchar(50) not null,
-	`password` varchar(50) not null,
-	`email` varchar(70) not null,
-	`profilename` varchar(50) not null,
-	`max_no_paper` int(11) DEFAULT 1 
-);
-
-DROP TABLE IF EXISTS `conference`;
-CREATE TABLE `conference` (
-	`username` varchar(50) PRIMARY KEY not null,
-	`fullname` varchar(50) not null,
-	`password` varchar(50) not null,
-	`email` varchar(70) not null,
-	`profilename` varchar(50) not null
-);
-
-DROP TABLE IF EXISTS `author`;
-CREATE TABLE `author` (
-	`username` varchar(50) PRIMARY KEY not null,
-	`fullname` varchar(50) not null,
-	`password` varchar(50) not null,
-	`email` varchar(70) not null,
-	`profilename` varchar(50) not null
-);
-
 DROP TABLE IF EXISTS `userprofile`;
 CREATE TABLE `userprofile` (
-	`profilename` varchar(50) PRIMARY KEY not null,
+	`profileID` int(11) primary key auto_increment, 
+	`profilename` varchar(50) not null,
 	`description` varchar(50) not null
 );
 
 
-INSERT INTO `systemadmin` (`username`,`fullname`, `password`, `email` , `profileName`) VALUES
-('admin', 'Admin1', 'admin', 'admin1@asda.com', "SystemAdmin"),
-('reviewer', 'Reviewer1', 'reviewer', 'reviewer1@asda.com', "SystemAdmin"),
-('cc', 'CC', 'cc', 'cc@asda.com', "SystemAdmin"),
-('author', 'Author', 'author', 'author1@asda.com', "SystemAdmin");
+INSERT INTO `userprofile` (`profileID`,`profilename`,`description`) VALUES
+(1,"SystemAdmin", "Manage User Accounts and User Profile"),
+(2,"Reviewer", "Review Book"),
+(3,"Conference", "Allocate papers to reviewer"),
+(4,"Author", "Publish his/her paper to be reviewed");
 
-INSERT INTO `reviewer` (`username`,`fullname`, `password`, `email` , `profileName`, `max_no_paper`) VALUES
-('admin', 'Admin1', 'admin', 'admin1@asda.com', "Reviewer", 1),
-('reviewer', 'Reviewer1', 'reviewer', 'reviewer1@asda.com', "Reviewer", 3),
-('cc', 'CC', 'cc', 'cc@asda.com', "Reviewer", 4),
-('author', 'Author', 'author', 'author1@asda.com', "Reviewer", 2);
+DROP TABLE IF EXISTS `systemadmin`;
+CREATE TABLE `systemadmin` (
+	`id` int(11) primary key auto_increment, 
+	`username` varchar(50) not null,
+	`fullname` varchar(50) not null,
+	`password` varchar(50) not null,
+	`email` varchar(70) not null,
+	`profileID` int(11) not null,
+	 FOREIGN KEY fk_systemadmin(`profileID`) REFERENCES userprofile(profileID)
+	
+);
 
-INSERT INTO `conference` (`username`,`fullname`, `password`, `email` , `profileName`) VALUES
-('admin', 'Admin1', 'admin', 'admin1@asda.com', "Conference"),
-('reviewer', 'Reviewer1', 'reviewer', 'reviewer1@asda.com', "Conference"),
-('cc', 'CC', 'cc', 'cc@asda.com', "Conference"),
-('author', 'Author', 'author', 'author1@asda.com', "Conference");
+DROP TABLE IF EXISTS `reviewer`;
+CREATE TABLE `reviewer` (
+	`id` int(11) primary key auto_increment, 
+	`username` varchar(50)  not null,
+	`fullname` varchar(50) not null,
+	`password` varchar(50) not null,
+	`email` varchar(70) not null,
+	`profileID` int(11),
+	`max_no_paper` int, 
+	 FOREIGN KEY fk_reviewer(`profileID`) REFERENCES userprofile(profileID)
+);
 
-INSERT INTO `author` (`username`,`fullname`, `password`, `email` , `profileName`) VALUES
-('admin', 'Admin1', 'admin', 'admin1@asda.com', "Author"),
-('reviewer', 'Reviewer1', 'reviewer', 'reviewer1@asda.com', "Author"),
-('cc', 'CC', 'cc', 'cc@asda.com', "Author"),
-('author', 'Author', 'author', 'author1@asda.com', "Author");
+DROP TABLE IF EXISTS `conference`;
+CREATE TABLE `conference` (
+	`id` int(11) primary key auto_increment, 
+	`username` varchar(50) not null,
+	`fullname` varchar(50) not null,
+	`password` varchar(50) not null,
+	`email` varchar(70) not null,
+	`profileID` int(11),
+	 FOREIGN KEY fk_conference(`profileID`) REFERENCES userprofile(profileID)
+);
 
-INSERT INTO `userprofile` (`profilename`,`description`) VALUES
-("SystemAdmin", "Manage User Accounts and User Profile"),
-("Reviewer", "Review Book"),
-("Conference", "Allocate papers to reviewer"),
-("Author", "Publish his/her paper to be reviewed");
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE `author` (
+	`id` int(11) primary key auto_increment, 
+	`username` varchar(50)  not null,
+	`fullname` varchar(50) not null,
+	`password` varchar(50) not null,
+	`email` varchar(70) not null,
+	`profileID` int(11),
+	 FOREIGN KEY fk_author(`profileID`) REFERENCES userprofile(profileID)
+);
+
+
+INSERT INTO `systemadmin` (`username`,`fullname`, `password`, `email` , `profileID`) VALUES
+('admin', 'Admin1', 'admin', 'admin1@asda.com', 1),
+('reviewer', 'Reviewer1', 'reviewer', 'reviewer1@asda.com', 1),
+('cc', 'CC', 'cc', 'cc@asda.com', 1),
+('author', 'Author', 'author', 'author1@asda.com', 1);
+
+INSERT INTO `reviewer` (`username`,`fullname`, `password`, `email` , `profileID`, `max_no_paper`) VALUES
+('admin', 'Admin1', 'admin', 'admin1@asda.com', 2, 2),
+('reviewer', 'Reviewer1', 'reviewer', 'reviewer1@asda.com', 2, 5),
+('cc', 'CC', 'cc', 'cc@asda.com', 2, 4),
+('author', 'Author', 'author', 'author1@asda.com', 2, 3);
+
+INSERT INTO `conference` (`username`,`fullname`, `password`, `email` , `profileID`) VALUES
+('admin', 'Admin1', 'admin', 'admin1@asda.com', 3),
+('reviewer', 'Reviewer1', 'reviewer', 'reviewer1@asda.com', 3),
+('cc', 'CC', 'cc', 'cc@asda.com', 3),
+('author', 'Author', 'author', 'author1@asda.com', 3);
+
+INSERT INTO `author` (`username`,`fullname`, `password`, `email` , `profileID`) VALUES
+('admin', 'Admin1', 'admin', 'admin1@asda.com', 4),
+('reviewer', 'Reviewer1', 'reviewer', 'reviewer1@asda.com', 4),
+('cc', 'CC', 'cc', 'cc@asda.com', 4),
+('author', 'Author', 'author', 'author1@asda.com', 4);
+
+DROP TABLE IF EXISTS `paper`;
+CREATE TABLE `paper` (
+	`paper_id` int not null auto_increment primary key,
+	 paperName varchar(50) not null,
+	`fileContent` blob not null,
+	`Status` varchar(50)not null
+);
+
+DROP TABLE IF EXISTS `paperinfo`;
+CREATE TABLE `paperinfo` (
+	 `paper_id` int not null auto_increment primary key,
+	`paperidfk` int(11) not null,
+	`Author` int(11) not null,
+	 FOREIGN KEY fk_authorname(`Author`) REFERENCES author(id),
+	 FOREIGN KEY fk_paper(`paperidfk`) REFERENCES paper(paper_id)
+);
+
+DROP TABLE IF EXISTS `reviews`;
+CREATE TABLE `reviews` (
+	 review_id int not null auto_increment primary key,
+	`reviewContent` varchar(50) not null,
+	`paperidfk` int(11) not null,
+	`reviewer` int(11) not null,
+    `rating` int not null,
+	 FOREIGN KEY fk_reviewername(`reviewer`) REFERENCES reviewer(id),
+	 FOREIGN KEY fk_papername(`paperidfk`) REFERENCES paper(paper_id)
+);
+
+DROP TABLE IF EXISTS `bid`;
+CREATE TABLE `bid` (
+	 bid_id int not null auto_increment primary key,
+	`reviewName` int(11) not null,
+	`paperidfk` int(11) not null,
+	 FOREIGN KEY fk_reviewbidname(`reviewName`) REFERENCES reviewer(id),
+	 FOREIGN KEY fk_paperbidname(`paperidfk`) REFERENCES paper(paper_id)
+);
+
+DROP TABLE IF EXISTS `allocation`;
+CREATE TABLE `allocation` (
+	 allocation_id int not null auto_increment primary key,
+	`reviewName` int(11) not null,
+	`paperidfk` int(11) not null,
+	 FOREIGN KEY fk_reviewallocatename(`reviewName`) REFERENCES reviewer(id),
+	 FOREIGN KEY fk_paperallocatename(`paperidfk`) REFERENCES paper(paper_id)
+);
+
+
+
+
+
+
+
+
 
 
 
