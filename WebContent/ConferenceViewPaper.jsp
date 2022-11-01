369b3171e4198313beefb8d1419ecf7703f6821c
limlyang121 <103249985+limlyang121@users.noreply.github.com>
2022-11-02 00:31:27 +0800

    CC can now view Accept/Reject/Pending Status

--- WebContent/ConferenceViewPaper.jsp
+++ WebContent/ConferenceViewPaper.jsp
@@ -7,9 +7,15 @@
 <body>
 	<%@include file = "include/navbar.jsp" %>
 	
+	
 	<h1>Current Bid from reviewer</h1>
 	
-	<table>
+
+<a href="<%=request.getContextPath()%>/viewAllPaper?status=Pending" > <button> View Pending </button> </a>
+<a href="<%=request.getContextPath()%>/viewAllPaper?status=Accept"> <button> View Accept </button> </a>	
+<a href="<%=request.getContextPath()%>/viewAllPaper?status=Reject"> <button> View Reject </button> </a>
+<div align = center>
+	<table border = 1 cellpadding = "10">
 		<thead>
 			<tr>
 				<th>PaperID</th>
@@ -25,6 +31,7 @@
 		
 		<tbody> 
 			<c:forEach var ="paperInfo" items = "${paperInfo}">
+				<c:if test = "${param.status == null || param.status == paperInfo.status}"> 
 				<tr>
 					<td>
 						<c:out value="${paperInfo.id}" />
@@ -63,6 +70,7 @@
 					</td>
 				
 				</tr>
+				</c:if>
 			
 			
 			</c:forEach>
@@ -70,5 +78,6 @@
 		
 		</tbody>
 	</table>
+</div>
 </body>
 </html>
---
+++ src/main/java/Paper/Controller/ViewSpecifiedPaperController.java
@@ -0,0 +1,30 @@
+package Paper.Controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Paper.Entity.Paper;
+
+@WebServlet ("/viewPendingPaper")
+public class ViewSpecifiedPaperController {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ViewAllPaper (request, response);
+    }
+    
+    protected void ViewAllPaper(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Paper temp = new Paper();
+        ArrayList<Paper> allPaper = temp.getAllPapers();
+        request.setAttribute("paperInfo", allPaper);
+        RequestDispatcher dis = request.getRequestDispatcher("/ConferenceViewPaper.jsp");
+        dis.forward(request, response);
+        
+    }
+}
